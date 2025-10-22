70f3bcaa4b8fe2cb230719260e9da7d3f8cbe641
max-sliva <persimon@inbox.ru>
2025-10-23 00:01:27 +0500

    updated class for window

--- app/src/main/java/ArduinoFiles/smartLabTest/SmartWindow.h
+++ app/src/main/java/ArduinoFiles/smartLabTest/SmartWindow.h
@@ -1,1 +1,15 @@
+class SmartWindow{
+    private:
+        byte motorPin1;
+        byte motorPin2;
+        byte potPin;
+        byte windowOpenedVal;
+        byte windowClosedVal;
+        byte windowCurVal;
+        bool windowIsOpened;
+    public:
+        SmartWindow(){
 
+        }
+
+};
