737607ad4fbc7335ef289d6e08c47e9a688cf5f0
HoduJoeck <idvdtriples@gmail.com>
2026-06-16 15:29:26 +0500

    updated door interface
    Nextion & MainC

--- app/src/main/java/ArduinoFiles/Serial/I2C REBUILD/mega_03/mega_03.ino
+++ app/src/main/java/ArduinoFiles/Serial/I2C REBUILD/mega_03/mega_03.ino
@@ -19,6 +19,7 @@ const char* names[10] = {"h0.val","h1.val","h2.val","va0.val",
 const char* w_names[2] = {"h1.val","h2.val"};
 uint16_t colors[4] = {36415,1055,50712,31727};
 uint8_t values[4] = {0xFF,0xFF,0xFF,0};
+boolean d_values[3] = {0,0,0};
 boolean extPower = false;
 uint8_t colNum;
 uint32_t time_ms = 0;
@@ -56,6 +57,8 @@ void packageHandler(uint8_t comma, uint8_t data) {
         mySerial.sendPackageExtra(0,length);
       }
       break;
+    case 10:
+
     default:
       break;
   }
@@ -178,19 +181,33 @@ void trigger3() {
 }
 
 void trigger4() {
-
+  myNex.writeNum("va0.val", d_values[1]);
+  if (d_values[2] == HIGH) {
+    mynex.writeStr("vis t0,1");
+    mynex.writeStr("vis b5,1");
+    mynex.writeStr("vis b6,1");
+  }
 }
 
 void trigger5() {
-  
+  if (myNex.readNumber("va1.val")!=d_value[0]) {
+    d_value[0] = !d_value[0];
+    mySerial.sendPackage(10, d_value[0]);
+  } 
 }
 
 void trigger6() {
-  
+  if (myNex.readNumber("va2.val")!=d_value[1]) {
+    d_value[1] = !d_value[1];
+    mySerial.sendPackage(11, d_value[1]); 
+  }
 }
 
 void trigger7() {
-  
+  if (myNex.readNumber("va2.val")!=d_value[2]) {
+    d_value[2] = !d_value[2];
+    mySerial.sendPackage(12, d_value[2]);
+  }
 }
 
 void trigger9() {
